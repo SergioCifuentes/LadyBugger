acdc1af0d44ed4a00adcbed8c97e3d966e347fe1
Sergio CIfuentes <sergio@sergio-HP-Laptop-15-bs0xx.local.tld>
2022-03-11 22:25:23 -0500

    Project Creation

---
+++ src/main/java/ladybugger/LadyBuggerApplication.java
@@ -0,0 +1,13 @@
+package ladybugger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadyBuggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadyBuggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ladybugger/LadyBuggerApplicationTests.java
@@ -0,0 +1,13 @@
+package ladybugger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadyBuggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
