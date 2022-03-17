ea49c3231a9a4a2a89fbe5d883dc0d370f8f07ed
Marito-R-T <mariomoises20008@gmail.com>
2022-03-17 15:47:18 -0600

    Login prueba

--- src/main/java/ladybugger/LadyBuggerApplication.java
+++ src/main/java/ladybugger/LadyBuggerApplication.java
@@ -2,6 +2,8 @@ package ladybugger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LadyBuggerApplication {
@@ -9,5 +11,6 @@ public class LadyBuggerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LadyBuggerApplication.class, args);
 	}
+
 
 }
---
+++ src/main/java/ladybugger/login/Login.java
@@ -0,0 +1,36 @@
+package ladybugger.login;
+
+public class Login {
+
+    private String email;
+    private String password;
+
+    public Login(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Login{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
