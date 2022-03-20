186ce6030122baf959952fa9b69583023934686f
Sergio CIfuentes <sergio@sergio-HP-Laptop-15-bs0xx.local.tld>
2022-03-20 13:50:49 -0400

    remove filter

--- src/main/java/ladybugger/security/WebSecurityConfig.java
+++ src/main/java/ladybugger/security/WebSecurityConfig.java
@@ -54,6 +54,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
 			.anyRequest().authenticated();
-		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+		;
 	}
 }
