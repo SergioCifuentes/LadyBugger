920609d5056c8666fa03b4b84cba3150c2efe761
Marito-R-T <mariomoises20008@gmail.com>
2022-03-29 19:09:48 -0600

    merge2

--- src/main/java/ladybugger/LadyBuggerApplication.java
+++ src/main/java/ladybugger/LadyBuggerApplication.java
@@ -2,8 +2,6 @@ package ladybugger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
